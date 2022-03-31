51c474e2c5cbb7a995988378957d578d71c985ee
Olalekan <padinality@yahoo.com>
2022-03-31 07:22:10 +0100

    Add migrations and urls

--- noun/models.py
+++ noun/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Noun(models.Model):
+    name = models.CharField(max_length=64)
+    place = models.CharField(max_length=64)
+    animal = models.CharField(max_length=64)
+    food = models.CharField(max_length=64)
+    things = models.CharField(max_length=64)

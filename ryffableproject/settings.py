b9dccb596af3a4c81cb78a6e78e0d3bc31d6c7d8
Olalekan <padinality@yahoo.com>
2022-03-31 14:36:10 +0100

    Remove secret key from settings file

--- ryffableproject/settings.py
+++ ryffableproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u(o(^*-7r!0i^g)j3_1%n(rd6fkqi%xqe!dum=3_hcur0og0d0'
+# SECRET_KEY = 'django-insecure-u(o(^*-7r!0i^g)j3_1%n(rd6fkqi%xqe!dum=3_hcur0og0d0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

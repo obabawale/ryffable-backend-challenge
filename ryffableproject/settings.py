62337aa3ebee2479f09b21fc43ee1ba03b4d0e32
Olalekan <padinality@yahoo.com>
2022-03-31 14:08:50 +0100

    Automatic configuration with django-heroku

--- ryffableproject/settings.py
+++ ryffableproject/settings.py
@@ -133,3 +133,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+import django_heroku
+django_heroku.settings(locals())

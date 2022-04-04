b260e9b5d35c4a68f8f28f9349de6d41e84a4fde
Olalekan <padinality@yahoo.com>
2022-04-04 12:25:48 +0100

    Add swagger yasg

--- ryffableproject/settings.py
+++ ryffableproject/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_extensions',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- ryffableproject/urls.py
+++ ryffableproject/urls.py
@@ -3,6 +3,23 @@ from django.urls import path, include
 from rest_framework import routers
 from noun import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ryffable Takehome API Task",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 router = routers.DefaultRouter()
 router.register(r'nouns', views.NounViewSet)
 router.register(r'places', views.PlaceViewSet)
@@ -12,5 +29,7 @@ router.register(r'things', views.ThingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
